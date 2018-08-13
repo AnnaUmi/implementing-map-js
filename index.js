ef900bfdffbf2148394fed9c86e270df25fb1781
Anna Vlasenko <anna_vlasenko@epam.com>
2018-08-13 14:14:07 +0300

    __proto__ using

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const createMap = require('./map');
 
-let phones = createMap();
-let contacts = createMap();
+let phones = new createMap();
+let contacts = new createMap();
 
 phones.set('model', 'iPhone').set('madeBy', 'Apple').set('madeIn', 'China');
 contacts.set('Anna Vlasenko', 'email@annavlasenko.com').set('Spiderman', 'email@spiderman.com');
 
-console.log(contacts.keys)
-for(let key of phones){
-    console.log(key)
-}
+console.log(phones)
--- map.js
+++ map.js
@@ -1,61 +1,65 @@
 
-	const mapPrototype = {
-		get size(){
-			return Object.keys(this.store).length
-			
-		},
-		set size(value){
-			if (value === 0) this.clear()
-		},
-		get keys(){
-			return Object.keys(this.store)
-		},
-		get values(){
-			return Object.values(this.store)
-		},
-		get entries(){
-			return Object.entries(this.store)
-		},
-		has(key){
-			return key in this.store
-		},
-		get(key){
-			return this.store[key]
-		},
-		set(key, value){
-			this.store[key] = value;
-			return this
-		},
-		delete(key){
-			return delete this.store[key]
-		},
-		clear(){
-			this.store = {}
-		},
-		forEach(cb){
-			for(let key in this.store){
-				cb(this.store[key], key, this)
-			}
-		},
-		[Symbol.iterator](){
-			let values = this.values.slice();
-				return{
-					next(){
-						let value = values.shift();
-							return {
-								value,
-								done: Boolean(value) ? false : true
-							};
-					}
-				};
+const mapPrototype = {
+	get size() {
+		return Object.keys(this.store).length
 
+	},
+	set size(value) {
+		if (value === 0) this.clear()
+	},
+	get keys() {
+		return Object.keys(this.store)
+	},
+	get values() {
+		return Object.values(this.store)
+	},
+	get entries() {
+		return Object.entries(this.store)
+	},
+	has(key) {
+		return key in this.store
+	},
+	get(key) {
+		return this.store[key]
+	},
+	set(key, value) {
+		this.store[key] = value;
+		return this
+	},
+	delete(key) {
+		return delete this.store[key]
+	},
+	clear() {
+		this.store = {}
+	},
+	forEach(cb) {
+		for (let key in this.store) {
+			cb(this.store[key], key, this)
 		}
-	}
-	function createMap(){
-		let map = {
-			store: {}
+	},
+	[Symbol.iterator]() {
+		let values = this.values.slice();
+		return {
+			next() {
+				let value = values.shift();
+				return {
+					value,
+					done: Boolean(value) ? false : true
+				};
+			}
 		};
-		Object.defineProperties(map, Object.getOwnPropertyDescriptors(mapPrototypen))
-		return map
-		}
+
+	}
+}
+createMap.prototype = mapPrototype;
+//function copy(source, target) {
+//	return Object.defineProperties(target, Object.getOwnPropertyDescriptors(source))
+//}
+function createMap() {
+	const map = {
+		store: {},
+		__proto__: createMap.prototype
+	};
+	return map
+}
 module.exports = createMap;
